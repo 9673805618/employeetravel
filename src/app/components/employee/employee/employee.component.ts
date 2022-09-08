f717f4c660d9bcecf7e5bc5d3aa9f8d7dae68f98
Siddhesh  kumbhar <106369224+9673805618@users.noreply.github.com>
2022-09-08 20:03:04 +0530

    Add files via upload

---
+++ src/app/components/employee/employee/employee.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Login } from 'src/app/domain/login';
+import { EmployeeDetailsService } from 'src/app/services/employee-details.service';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+
+  login:Login=new Login();
+  constructor(private employeeDetailService : EmployeeDetailsService,
+    private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/employee/view-existing-request/view-existing-request.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-existing-request',
+  templateUrl: './view-existing-request.component.html',
+  styleUrls: ['./view-existing-request.component.css']
+})
+export class ViewExistingRequestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
